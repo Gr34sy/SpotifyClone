a4ced8b0e27a3031a31127c67538d924447dd849
Gr34sy <slupek3@poczta.fm>
2022-12-15 17:47:16 +0100

    added searching

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { Container } from 'react-bootstrap'
+import React, {useState, useEffect} from 'react';
+import { Container, Form } from 'react-bootstrap'
 import useAuth from '../useAuth';
+import SpotifyWebApi from 'spotify-web-api-node';
+import TrackSearchResult from './TrackSearchResult';
 
-const AUTH_URL = "https:/accounts.spotify.com/authorize?client_id=e3ba317d53ef47979e15c96a2369f2c7&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'e3ba317d53ef47979e15c96a2369f2c7',
+})
 
 export default function Dashboard({code}){
     const accessToken  = useAuth(code)
+    const [search, setSearch] = useState("");
+    const [searchResults, setSearchResults] = useState([])
+
+
+    useEffect(()=>{
+        if(!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    },[accessToken])
+
+    useEffect(()=>{
+        if(!search) return setSearchResults([])
+        if(!accessToken) return 
+
+        let cancel = false
+        spotifyApi.searchTracks(search)
+        .then(res => {
+            if(cancel) return
+            setSearchResults(res.body.tracks.items.map( track => {
+                const smallestAlbumImage = track.album.images.reduce((smallest, image) =>{
+                    if(image.height < smallest.height) return image
+                    return smallest
+                }, track.album.images[0])
+                return{
+                    artist: track.artists[0].name,
+                    title: track.name,
+                    uri: track.uri,
+                    albumUrl: smallestAlbumImage.url
+                }
+            }))
+        })
+
+        return () => cancel = true
+    }, [search, accessToken])
+
     return(
-        <Container>
-            {code}
+        <Container 
+        className='d-flex flex-column py-2' 
+        style={{height: '100vh'}}>
+            <Form.Control 
+            type="search" 
+            placeholder="Search Songs or Artists" 
+            value={search} 
+            onChange={e => setSearch(e.target.value)} 
+            />
+            <div className='flex-grow-1 my-2' style={{overflowY: 'auto'}}>
+                {searchResults.map(
+                    track =>(
+                    <TrackSearchResult
+                    track={track}
+                    key={track.uri}
+                    />)
+                )}
+            </div>
+            <div>
+                Bottom
+            </div>
         </Container>
     )
 }                           
---
+++ client/src/components/TrackSearchResult.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+export default function TrackSearchResult({track}){
+    return(
+        <Container className='d-flex m-2 align-items-center' style={{cursor: 'pointer'}}>
+            <img src={track.albumUrl} style={{height: '64px', width: '64px'}}/>
+            <div className='ms-3'>
+                <div>{track.title}</div>
+                <div className='text-muted'>
+                    {track.artist}
+                </div>
+            </div>
+        </Container>
+    )
+}
